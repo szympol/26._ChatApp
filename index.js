ef6e4ba5e73f834f6ae1df90e8f7c62789053237
Szymon Polanowski <szympol@hotmail.com>
2019-03-07 18:50:00 +0100

    Add backend config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const UsersService = require('./UsersService');
+
+const usersService = new UsersService();
 
 const app = express();
 const server = http.createServer(app);
@@ -9,9 +12,34 @@ const io = socketIo(server);
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+    socket.on('join', (name) => {
+        usersService.addUser({
+            id: socket.id,
+            name
+        });
+        io.emit('update', {
+            users: usersService.getAllUsers()
+        });
+    });
+    socket.on('disconnect', () => {
+        usersService.removeUser(socket.id);
+        socket.broadcast.emit('update', {
+            users: usersService.getAllUsers()
+        });
+    });
+    socket.on('message', (message) => {
+        const {name} = usersService.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text: message.text,
+            from: name
+        });
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
