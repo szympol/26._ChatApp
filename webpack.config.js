657cfc235c2362e57851ee50430fdf9187993961
Szymon Polanowski <szympol@hotmail.com>
2019-03-08 10:43:41 +0100

    Add npm config

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
+
+module.exports = (env) => {
+  const environment =  env || 'production';
+  if (env === 'production') {
+    plugins.push(
+      new UglifyJSPlugin({
+        test: /\.js($|\?)/i
+    }),
+      new OptimizeJsPlugin({
+        sourceMap: false
+    })
+    )
+  }
+  return {
+    mode: environment,
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'app.bundle.js'
+  },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {loader: 'style-loader'},
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimize: true
+    },
+    devServer: {
+      proxy: {
+          '/socket.io': {
+              target: 'http://localhost:3000',
+              ws: true
+          }
+      }
+    },
+    plugins
+  }
+};
+
+
+
+
+
+devServer: {
+  proxy: {
+      '/socket.io': {
+          target: 'http://localhost:3000',
+          ws: true
+      }
+  }
+}
